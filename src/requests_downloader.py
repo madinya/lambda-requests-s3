7b971bfc1856d39edeb55dc2de3a59ca971a7022
Roberto Madinyá <roberto.madinya@ioet.com>
2023-08-07 19:34:49 -0500

    refactor code to use classes (#8)

---
+++ src/requests_downloader.py
@@ -0,0 +1,10 @@
+import requests
+from .downloader import Downloader
+
+class RequestsDownloader(Downloader):
+    @classmethod
+    def download(cls, **kwargs):
+        url = kwargs["url"]
+        if not url: 
+            raise Exception('Url were not provided')
+        return requests.get(url)
